b7da4e0e314dd34b03ceff24ff96f2c1c30e8fd9
HydraMikeMudd <mikemudd29@gmail.com>
2023-06-11 14:24:55 -0500

    Reorganizing main file

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-from core.plant import Plant
+""" Main file for the DLPlant Discord Bot. Execute to run the bot."""
 import os
+import discord
+import dotenv
+
+from core.plant import Plant
 from core.sql_engine import SQLEngine
 from core.sql_engine import UserDetails
 from core.image_loader import load_image_path
 from core.image_loader import remove_image_path
-import discord
-import dotenv
 
 dotenv.load_dotenv()
 
@@ -15,7 +17,17 @@ tree = discord.app_commands.CommandTree(client)
 
 
 @tree.command(name="share", description="Shares your plant with the server.")
-async def show_plant(ctx):
+async def share_plant(ctx):
+    """
+    This function shares the plant with the server only if the user has a plant and it is not growing.
+    Plant image is retrieved from disk and sent as a discord embed. User information is retrieved from the database.
+
+    Args:
+        ctx: Discord Interation context.
+    
+    Returns:
+        None
+    """
     member_id = ctx.user.id
     guild_id = 0
     if ctx.guild:
@@ -39,6 +51,17 @@ async def show_plant(ctx):
 
 @tree.command(name="show", description="Shows only you your plant. If plant does not exist, creates one.")
 async def show_plant(ctx):
+    """
+    This function shares the plant with the user ephemerally only if the user has a plant and it is not growing.
+    Constructs a new plant if the user does not have a plant. Plant image is retrieved from disk and sent as a discord embed. 
+    User information is retrieved from the database.
+
+    Args:
+        ctx: Discord Interation context.
+    
+    Returns:
+        None
+    """
     member_id = ctx.user.id
     guild_id = 0
     if ctx.guild:
@@ -65,6 +88,16 @@ async def show_plant(ctx):
 
 @tree.command(name="grow", description="Grows your plant by one iteration.")
 async def grow_plant(ctx):
+    """
+    This function flags a user's plant to grow by one iteration on the MYSQL database.
+    If the plant has already grown to its maximum, the user is notified.
+
+    Args:
+        ctx: Discord Interation context.
+    
+    Returns:
+        None
+    """
     member_id = ctx.user.id
     guild_id = 0
     if ctx.guild:
@@ -73,16 +106,23 @@ async def grow_plant(ctx):
     user_details = conn.get_user(guild_id=guild_id, member_id=member_id)
     if not user_details:
         await ctx.response.send_message("You do not have a plant. Please create one with /show", ephemeral=True)
-        return
     elif user_details[0] > 6:
         await ctx.response.send_message("Your plant is fully grown.", ephemeral=True)
     else:
         conn.set_user_grow(guild_id=guild_id, member_id=member_id)
         await ctx.response.send_message("Your plant is beginning to grow. Check back in some time.", ephemeral=True)
-        return
 
 @tree.command(name="delete", description="Deletes your existing plant.")
 async def reset_plant(ctx):
+    """
+    This function removes a user from the MYSQL database.
+
+    Args:
+        ctx: Discord Interation context.
+    
+    Returns:
+        None
+    """
     member_id = ctx.user.id
     guild_id = 0
     if ctx.guild:
@@ -94,14 +134,15 @@ async def reset_plant(ctx):
         remove_image_path(user_details[UserDetails.IMG_PATH])
         conn.delete_user(guild_id=guild_id, member_id=member_id)
         await ctx.response.send_message("Your plant has been deleted.", ephemeral=True)
-        return
     else:
         await ctx.response.send_message("You do not have a plant. Please create one with /show", ephemeral=True)
-        return
 
 
 @client.event
 async def on_ready():
+    """
+    This function is called on bot start up and syncs slash commands.
+    """
     await tree.sync()
     print("Ready")
 
